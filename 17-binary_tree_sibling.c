0173e1a4b02eec41ee1be4757097f7f792c1c691
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-11-30 00:28:10 +0100

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -4,8 +4,8 @@
  * binary_tree_sibling - Finds the sibling of a node in a binary tree
  * @node: Pointer to the node whose sibling is to be found
  *
- * Return: Pointer to the sibling node, or NULL if node is NULL or has no parent
- *         or if it's the only child of its parent
+ * Return: Pointer to the sibling node, or NULL if node is NULL or
+ * has no parent or if it's the only child of its parent
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
