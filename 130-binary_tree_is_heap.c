2d21d8bb2c03f86883836602ff772f2d8709fc82
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-11-30 20:42:12 +0100

    130-binary_tree_is_heap.c

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -7,11 +7,10 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes);
 size_t binary_tree_size(const binary_tree_t *tree);
 
 /**
- * binary_tree_is_heap - Checks if a binary tree is a valid Max Binary Heap.
+ * binary_tree_is_heap - Checks if a binary tree follows the heap structure
+ * @tree: Pointer to the root of the binary tree
  *
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is a valid Max Binary Heap, and 0 otherwise.
+ * Return: 1 if the tree meets the heap structure, 0 otherwise.
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
@@ -22,11 +21,12 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 }
 
 /**
- * check_parent - Checks if parent has a greater value than its childs.
+ * check_parent - Checks if the parent node values are greater than
+ * their children's values
+ * @tree: Pointer to the root of the binary tree
  *
- * @tree: A pointer to the node.
- *
- * Return: 1 if parent has a greater value, 0 otherwise
+ * Return: 1 if the parent values are greater than or equal to their children's values,
+ * 0 otherwise.
  */
 int check_parent(const binary_tree_t *tree)
 {
@@ -40,10 +40,10 @@ int check_parent(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_complete - Calls to tree_is_complete function.
+ * binary_tree_is_complete - Checks if a binary tree is a complete binary tree
+ * @tree: Pointer to the root of the binary tree
  *
- * @tree: Tree root.
- * Return: 1 if tree is complete, 0 otherwise
+ * Return: 1 if the tree is a complete binary tree, 0 otherwise.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
@@ -58,13 +58,12 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 }
 
 /**
- * tree_is_complete - Checks if tree is complete.
+ * tree_is_complete - Checks if a binary tree satisfies the complete tree property
+ * @tree: Pointer to the root of the binary tree
+ * @i: Current index of the node being checked
+ * @cnodes: Total number of nodes in the tree
  *
- * @tree: Pointer to the tree root.
- * @i: Node index.
- * @cnodes: Number of nodes.
- *
- * Return: 1 if tree is complete, 0 otherwise
+ * Return: 1 if the tree satisfies the complete tree property, 0 otherwise.
  */
 int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 {
@@ -80,10 +79,10 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 
 
 /**
- * binary_tree_size - Measures the size of a binary tree.
+ * binary_tree_size - Measures the total number of nodes in a binary tree
+ * @tree: Pointer to the root node of the binary tree
  *
- * @tree: Tree root.
- * Return: Size of the tree or 0 if tree is NULL.
+ * Return: Total count of nodes in the binary tree; 0 if the tree is empty.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
